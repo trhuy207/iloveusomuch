6f684fbfeef0ab3faf15437ca37ff7a507418a5a
williamtran207 <quochuyphbrvt07@gmail.com>
2022-06-01 19:14:23 +0700

    upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hello Em Phụng Cute!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hủm!?',
+    title: 'Em đồng ý làm người iu anh nha! 😙',
+    desc: 'anh iu em nhiều lắm :< ',
+    btnYes: 'okla lun hí hí :3',
+    btnNo: 'Không, mơ hả pà già',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Phụng lại có tình cảm với tui nè <3',
+    btnReply: 'Gửi cho tui <3',
+    reply: 'Em gất thích Anh :> hãy là ny em nhé <33333333333333',
+    mess: 'Anh biết mà 🥰. Yêu Phụng nhiều nhiều 😘😘',
+    messDesc: 'Tối nay rảnh ib nhau nha :3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/quoc.huy.tran.2007/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
